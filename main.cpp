7ca57861ae980c47f8e7e4df06069fcb38bad639
ElectricFriez <36647231+ElectricFriez@users.noreply.github.com>
2018-05-23 08:43:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(string nameOfUser) {
+  cout << "So you've run into a bit of trouble there?" << endl;
+  system("pause");
+  cout << "Well I'm here to help!" << endl;
+  cout << "What did you say your name was?" << endl;
+  cin >> nameOfUser;
+  cout << string(50, '\n');
+  cout << "Alright then, " << nameOfUser << ". Shall we begin?" << endl;
+  system("pause")
+  
+}
+
